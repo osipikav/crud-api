ddd9b126785ef7aed938a8ccb00035e2f61a410b
Andrei Asipik <osipik.av@yandex.by>
2024-02-10 21:39:37 +0300

    feat: added post method

--- src/index.js
+++ src/index.js
@@ -24,6 +24,22 @@ function validUuid(uuid) {
   return uuidRegex.test(uuid);
 }
 
+function parseRequestBody(req, callback) {
+  let body = "";
+
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+  req.on("end", () => {
+    try {
+      const parseBody = JSON.parse(body);
+      callback(null, parseBody);
+    } catch (error) {
+      callback(error, null);
+    }
+  });
+}
+
 const server = http.createServer((req, res) => {
   const { url, method } = req;
 
@@ -33,13 +49,33 @@ const server = http.createServer((req, res) => {
     const userId = url.split("/")[3];
     const user = users.find((el) => el.id === userId);
 
-    if (userId || validUuid(userId)) {
+    if (!userId || !validUuid(userId)) {
       sendErrorResponse(res, 400, "Invalid user ID");
     } else if (!user) {
       sendErrorResponse(res, 404, "User not found");
     } else {
       sendResponse(res, 200, user);
     }
+  } else if (url.startsWith(baseUrl) && method === "POST") {
+    parseRequestBody(req, (error, body) => {
+      if (error) {
+        sendErrorResponse(res, 400, "Invalid request body");
+      } else {
+        if (!body.username || !body.age) {
+          sendErrorResponse(res, 400, "Missing required fields");
+        } else {
+          const newUserId = uuidv4();
+          const newUser = {
+            id: newUserId,
+            username,
+            age,
+            hobbies: hobbies || [],
+          };
+          users.push(newUser);
+          sendResponse(res, 201, newUser);
+        }
+      }
+    });
   } else {
     sendErrorResponse(res, 404, "Endpoint not found");
   }
